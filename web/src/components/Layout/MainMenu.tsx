631695e83f72664183b6b3b9739b1927d87f8762
jjb867 <joshbutkovic@gmail.com>
2019-07-22 19:29:14 -0700

    modifying main menu

--- web/src/components/Layout/MainMenu.tsx
+++ web/src/components/Layout/MainMenu.tsx
@@ -16,7 +16,7 @@ const MainMenu: React.FC = () => {
                     className="navbar-burger burger"
                     aria-label="menu"
                     aria-expanded="false"
-                    data-target="navbarBasicExample"
+                    data-target="main-menu-navbar"
                 >
                     <span aria-hidden="true" />
                     <span aria-hidden="true" />
@@ -24,34 +24,10 @@ const MainMenu: React.FC = () => {
                 </a>
             </div>
 
-            <div id="navbarBasicExample" className="navbar-menu">
+            <div id="main-menu-navbar" className="navbar-menu">
                 <div className="navbar-start">
                     <a className="navbar-item">Home</a>
-
                     <a className="navbar-item">Documentation</a>
-
-                    <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">More</a>
-
-                        <div className="navbar-dropdown">
-                            <a className="navbar-item">About</a>
-                            <a className="navbar-item">Jobs</a>
-                            <a className="navbar-item">Contact</a>
-                            <hr className="navbar-divider" />
-                            <a className="navbar-item">Report an issue</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                            <a className="button is-primary">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a className="button is-light">Log in</a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </nav>
